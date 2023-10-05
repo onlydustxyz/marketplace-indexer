33f6f22cdf6ecc89781de6aa8c03e1d383726ca3
Anthony Buisset <abuisset@gmail.com>
2023-10-05 07:57:26 +0200

    create other raw models

---
+++ domain/src/main/java/com/onlydust/marketplace/indexer/domain/model/Commit.java
@@ -0,0 +1,12 @@
+package com.onlydust.marketplace.indexer.domain.model;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor(force = true)
+public class Commit extends JsonDocument {
+}
---
+++ domain/src/main/java/com/onlydust/marketplace/indexer/domain/model/SocialAccount.java
@@ -0,0 +1,13 @@
+package com.onlydust.marketplace.indexer.domain.model;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor(force = true)
+public class SocialAccount extends JsonDocument {
+
+}
