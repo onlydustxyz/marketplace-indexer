914742ddc5976ceb3596f8c3b38e20a37b60ab99
Anthony Buisset <abuisset@gmail.com>
2023-10-05 07:47:28 +0200

    extract json document from user

---
+++ domain/src/main/java/com/onlydust/marketplace/indexer/domain/model/JsonDocument.java
@@ -0,0 +1,27 @@
+package com.onlydust.marketplace.indexer.domain.model;
+
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EqualsAndHashCode
+public class JsonDocument {
+    @Getter(value = AccessLevel.NONE)
+    Map<String, Object> properties = new HashMap<>();
+
+    @JsonAnySetter
+    public void add(String property, Object value) {
+        properties.put(property, value);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+}
--- domain/src/main/java/com/onlydust/marketplace/indexer/domain/model/User.java
+++ domain/src/main/java/com/onlydust/marketplace/indexer/domain/model/User.java
@@ -1,32 +1,14 @@
 package com.onlydust.marketplace.indexer.domain.model;
 
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import lombok.AccessLevel;
-import lombok.Getter;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Value
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor(force = true)
-public class User {
+public class User extends JsonDocument {
     Integer id;
     String login;
-
-    @Getter(value = AccessLevel.NONE)
-    Map<String, Object> properties = new HashMap<>();
-
-    @JsonAnySetter
-    public void add(String property, Object value) {
-        properties.put(property, value);
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getProperties() {
-        return properties;
-    }
 }
