b90bdb6ea182a1b2675e07193564c80538afa49d
Anthony Buisset <abuisset@gmail.com>
2023-10-08 10:13:49 +0200

    disable csrf

--- application/rest.api/src/main/java/com/onlydust/marketplace/indexer/rest/api/authentication/WebSecurityConfiguration.java
+++ application/rest.api/src/main/java/com/onlydust/marketplace/indexer/rest/api/authentication/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.onlydust.marketplace.indexer.rest.api.authentication;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,15 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @AllArgsConstructor
-public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-
-
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().cors();
+                .and().cors()
+                .and().csrf().disable()
+                .authorizeRequests().anyRequest().permitAll();
     }
 
 }
