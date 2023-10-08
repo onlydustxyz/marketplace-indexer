c1362b21ea02a4a4f2f15917b00e1ca3ca9fc2a2
Anthony Buisset <abuisset@gmail.com>
2023-10-08 22:50:45 +0200

    enhance integration test

--- bootstrap/src/test/java/com/onlydust/marketplace/indexer/bootstrap/ApplicationIT.java
+++ bootstrap/src/test/java/com/onlydust/marketplace/indexer/bootstrap/ApplicationIT.java
@@ -2,8 +2,10 @@ package com.onlydust.marketplace.indexer.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ApplicationIT {
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceIndexerApplication.class, args);
--- bootstrap/src/test/java/com/onlydust/marketplace/indexer/bootstrap/it/UserIndexingIT.java
+++ bootstrap/src/test/java/com/onlydust/marketplace/indexer/bootstrap/it/UserIndexingIT.java
@@ -1,12 +1,31 @@
 package com.onlydust.marketplace.indexer.bootstrap.it;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlydust.marketplace.indexer.domain.models.raw.RawUser;
+import com.onlydust.marketplace.indexer.postgres.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserIndexingIT extends IntegrationTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @BeforeEach
+    void setup() {
+        mapper.configure(DeserializationFeature.USE_LONG_FOR_INTS, true);
+    }
 
     @Test
-    public void should_index_user_on_demand() {
+    public void should_index_user_on_demand() throws IOException {
         // Given
         final Integer ANTHONY = 43467246;
 
@@ -15,6 +34,16 @@ public class UserIndexingIT extends IntegrationTest {
 
         // Then
         response.expectStatus().isNoContent();
+
+        final var expectedUser = mapper.readValue(getClass().getResourceAsStream("/github/users/anthony.json"), RawUser.class);
+
+        final var users = userRepository.findAll();
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users.get(0).getId()).isEqualTo(expectedUser.getId());
+        assertThat(users.get(0).getLogin()).isEqualTo(expectedUser.getLogin());
+        assertThat(users.get(0).getData()).isEqualTo(expectedUser);
+        assertThat(users.get(0).getCreatedAt()).isNotNull();
+        assertThat(users.get(0).getUpdatedAt()).isNotNull();
     }
 
     private WebTestClient.ResponseSpec indexUser(Integer userId) {
