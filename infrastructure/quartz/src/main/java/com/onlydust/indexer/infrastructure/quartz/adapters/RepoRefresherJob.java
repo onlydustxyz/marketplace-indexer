1481e214e14f005a70c9d40b97af1b17dd9350bb
Anthony Buisset <abuisset@gmail.com>
2023-10-12 21:04:14 +0200

    indexer job actually index repos

--- infrastructure/quartz/src/main/java/com/onlydust/indexer/infrastructure/quartz/adapters/RepoRefresherJob.java
+++ infrastructure/quartz/src/main/java/com/onlydust/indexer/infrastructure/quartz/adapters/RepoRefresherJob.java
@@ -1,26 +1,24 @@
 package com.onlydust.indexer.infrastructure.quartz.adapters;
 
+import com.onlydust.marketplace.indexer.domain.services.IndexingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+
+import java.util.HashSet;
 
 @Slf4j
 @AllArgsConstructor
 public class RepoRefresherJob implements Job {
+    private final IndexingService indexingService;
+
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
         final var installationId = context.getJobDetail().getJobDataMap().getLong("installationId");
         final var repos = context.getJobDetail().getJobDataMap().get("repos");
-        LOGGER.info("Refreshing repos for installation {} and repos {}", installationId, repos);
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        if (repos instanceof HashSet) {
+            ((HashSet<?>) repos).forEach(repo -> indexingService.indexRepo((Long) repo));
         }
-        LOGGER.info("Completed");
     }
-
-
 }
