3a877f212cd8fcc59ad03131f30fe27f45e4daf0
Olivier Fuxet <olivier.fuxet@gmail.com>
2023-10-24 17:58:00 +0200

    Fix migration

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000001_add_name_to_accounts.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000001_add_name_to_accounts.sql
@@ -2,8 +2,8 @@ create table indexer_exp.github_app_installations (
     id bigint primary key,
     account_id bigint not null references indexer_exp.github_accounts(id));
 
-insert into indexer_exp.github_app_installations
-    select installation_id, id from indexer_exp.github_accounts;
+insert into indexer_exp.github_app_installations (id, account_id)
+    select installation_id, id from indexer_exp.github_accounts where installation_id is not null;
 
 alter table indexer_exp.github_accounts
     add column name text,
