e3b6ed99967e330f0dc2850edb92bb5d02482b52
Anthony Buisset <abuisset@gmail.com>
2023-11-08 14:53:02 +0100

    improve string sanitization

--- infrastructure/github-api-adapter/src/test/java/com/onlydust/marketplace/indexer/github/SanitizerTest.java
+++ infrastructure/github-api-adapter/src/test/java/com/onlydust/marketplace/indexer/github/SanitizerTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SanitizerTest {
     @Test
     public void should_sanitize_content() {
-        final var sanitized = Sanitizer.sanitize("hello\n\\u0000\\u0000\\u0000\\u0000world".getBytes());
-        assertThat(sanitized).isEqualTo("hello\nworld");
+        final var sanitized = Sanitizer.sanitize("hello\\\\u0000\n\\u0000\\u0000\\u0000\\u0000world".getBytes());
+        assertThat(sanitized).isEqualTo("hello\\u0000\nworld");
     }
 }
