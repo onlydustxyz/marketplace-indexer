f330615eb54a83834d963a573d9a85717f827fdc
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-11-16 11:45:23 +0100

    [B-1126] Consider code review completed even if commented afterwards (#75)

--- domain/src/test/java/com/onlydust/marketplace/indexer/domain/stubs/ContributionStorageStub.java
+++ domain/src/test/java/com/onlydust/marketplace/indexer/domain/stubs/ContributionStorageStub.java
@@ -3,18 +3,20 @@ package com.onlydust.marketplace.indexer.domain.stubs;
 import com.onlydust.marketplace.indexer.domain.models.exposition.Contribution;
 import com.onlydust.marketplace.indexer.domain.ports.out.exposition.ContributionStorage;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContributionStorageStub implements ContributionStorage {
-    final List<Contribution> contributions = new ArrayList<>();
+    final Map<String, Contribution> contributions = new HashMap<>();
 
     @Override
     public void saveAll(Contribution... contributions) {
-        this.contributions.addAll(List.of(contributions));
+        Arrays.stream(contributions).forEach(c -> this.contributions.put(c.getId(), c));
     }
 
     public List<Contribution> contributions() {
-        return contributions;
+        return contributions.values().stream().toList();
     }
 }
