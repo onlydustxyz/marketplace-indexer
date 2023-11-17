2ddb816f66962e94f12c69041c294086d33a1ed1
Anthony Buisset <abuisset@gmail.com>
2023-11-17 17:35:00 +0100

    fix migration

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000019_add_account_installation_unique_constraint.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000019_add_account_installation_unique_constraint.sql
@@ -1,5 +1,14 @@
 -- delete all but the latest row for each account_id
 delete
+from indexer_exp.authorized_github_repos
+where installation_id in (select id
+                          from indexer_exp.github_app_installations
+                          except
+                          (select max(id)
+                           from indexer_exp.github_app_installations
+                           group by account_id));
+
+delete
 from indexer_exp.github_app_installations
 where id in (select id
              from indexer_exp.github_app_installations
