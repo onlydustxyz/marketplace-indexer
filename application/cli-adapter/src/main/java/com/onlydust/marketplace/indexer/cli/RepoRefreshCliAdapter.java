905353e8b20a88263ae22e42f4367df6a63f619b
Anthony Buisset <abuisset@gmail.com>
2023-11-21 10:52:45 +0100

    remove refresh job parallelization

--- application/cli-adapter/src/main/java/com/onlydust/marketplace/indexer/cli/RepoRefreshCliAdapter.java
+++ application/cli-adapter/src/main/java/com/onlydust/marketplace/indexer/cli/RepoRefreshCliAdapter.java
@@ -1,14 +1,12 @@
 package com.onlydust.marketplace.indexer.cli;
 
-import com.onlydust.marketplace.indexer.domain.exception.OnlyDustException;
+import com.onlydust.marketplace.indexer.domain.jobs.Job;
 import com.onlydust.marketplace.indexer.domain.ports.in.jobs.RepoRefreshJobManager;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.task.TaskExecutor;
-
-import java.util.concurrent.FutureTask;
 
 @AllArgsConstructor
 @Slf4j
@@ -20,14 +18,15 @@ public class RepoRefreshCliAdapter implements CommandLineRunner {
     @Override
     public void run(String... args) {
         LOGGER.info("Refreshing repos");
-        final var jobs = cachedRepoRefreshJobManager.allJobs().stream().map(j -> new FutureTask<>(j, null)).toList();
-        jobs.forEach(applicationTaskExecutor::execute);
-        jobs.forEach(j -> {
-            try {
-                j.get();
-            } catch (Exception e) {
-                throw OnlyDustException.internalServerError("Error refreshing expositions", e);
-            }
-        });
+//        final var jobs = cachedRepoRefreshJobManager.allJobs().stream().map(j -> new FutureTask<>(j, null)).toList();
+//        jobs.forEach(applicationTaskExecutor::execute);
+//        jobs.forEach(j -> {
+//            try {
+//                j.get();
+//            } catch (Exception e) {
+//                throw OnlyDustException.internalServerError("Error refreshing expositions", e);
+//            }
+//        });
+        cachedRepoRefreshJobManager.allJobs().forEach(Job::run);
     }
 }
