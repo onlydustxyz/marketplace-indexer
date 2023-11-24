985343dfd3a2ddb4cc668d0dad780f4a739ec48e
Anthony Buisset <abuisset@gmail.com>
2023-11-24 14:20:03 +0100

    split indexer jobs dyno from api

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: ./heroku/start-dyno-with-datadog-apm.sh
+web: ./heroku/start-dyno-with-datadog-apm.sh -Dspring.profiles.active=api,github -DDD_SERVICE=indexer-api
+jobs: ./heroku/start-dyno-with-datadog-apm.sh -Dspring.profiles.active=job -DDD_SERVICE=indexer-jobs
 cli: java -Dspring.profiles.active=local,cli -jar bootstrap/target/marketplace-indexer.jar
--- heroku/start-dyno-with-datadog-apm.sh
+++ heroku/start-dyno-with-datadog-apm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
-java -javaagent:dd-java-agent.jar -Dspring.profiles.active=api,github,job -XX:FlightRecorderOptions=stackdepth=256 -jar bootstrap/target/marketplace-indexer.jar
+java -javaagent:dd-java-agent.jar "$@" -XX:FlightRecorderOptions=stackdepth=256 -jar bootstrap/target/marketplace-indexer.jar
