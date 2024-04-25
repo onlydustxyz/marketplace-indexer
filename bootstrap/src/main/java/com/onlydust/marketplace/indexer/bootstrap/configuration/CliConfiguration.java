f6f99f9fdd66e7656c29934fae67196051f022c5
Anthony Buisset <abuisset@gmail.com>
2024-04-25 16:51:02 +0200

    create binary to re-index all commits

--- bootstrap/src/main/java/com/onlydust/marketplace/indexer/bootstrap/configuration/CliConfiguration.java
+++ bootstrap/src/main/java/com/onlydust/marketplace/indexer/bootstrap/configuration/CliConfiguration.java
@@ -1,7 +1,15 @@
 package com.onlydust.marketplace.indexer.bootstrap.configuration;
 
+import com.onlydust.marketplace.indexer.cli.CommitIndexerCliAdapter;
 import com.onlydust.marketplace.indexer.cli.RepoRefreshCliAdapter;
+import com.onlydust.marketplace.indexer.domain.ports.in.contexts.GithubAppContext;
 import com.onlydust.marketplace.indexer.domain.ports.in.jobs.JobManager;
+import com.onlydust.marketplace.indexer.domain.ports.out.raw.RawStorageReader;
+import com.onlydust.marketplace.indexer.postgres.repositories.RepoIndexingJobEntityRepository;
+import com.onlydust.marketplace.indexer.postgres.repositories.exposition.GithubPullRequestRepository;
+import com.onlydust.marketplace.indexer.postgres.repositories.raw.PullRequestCommitsRepository;
+import com.onlydust.marketplace.indexer.postgres.repositories.raw.PullRequestRepository;
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,5 +22,17 @@ public class CliConfiguration {
             final JobManager cacheOnlyRepoRefreshJobManager
     ) {
         return new RepoRefreshCliAdapter(cacheOnlyRepoRefreshJobManager);
+    }
+
+    @Bean
+    public CommitIndexerCliAdapter repoRefreshCliAdapter(final @NonNull PullRequestCommitsRepository pullRequestCommitsRepository,
+                                                         final @NonNull PullRequestRepository pullRequestRepository,
+                                                         final @NonNull RawStorageReader githubRawStorageReader,
+                                                         final @NonNull RepoIndexingJobEntityRepository repoIndexingJobEntityRepository,
+                                                         final @NonNull GithubAppContext githubAppContext,
+                                                         final @NonNull GithubPullRequestRepository githubPullRequestRepository
+    ) {
+        return new CommitIndexerCliAdapter(pullRequestCommitsRepository, pullRequestRepository, githubRawStorageReader, repoIndexingJobEntityRepository,
+                githubAppContext, githubPullRequestRepository);
     }
 }
