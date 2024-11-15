4f22cbb091e87c5db3053c9905d617146ddec4fa
Anthony Buisset <abuisset@gmail.com>
2024-11-15 18:08:38 +0100

    adapt promote script for aws

--- heroku/promote.sh
+++ heroku/promote.sh
@@ -3,22 +3,17 @@
 SCRIPT_DIR=$(readlink -f "$0" | xargs dirname)
 . "$SCRIPT_DIR"/utils.sh
 
-FROM_ENV=
-FROM_BRANCH=
-TO_ENV=
-TO_BRANCH=
-HEALTH_CHECK_URLS=()
-FORCE=0
-SKIP_DB_DUMP=0
+unset -v FROM_BRANCH
+unset -v TO_BRANCH
+unset -v GIT_FORCE
 
 REMOTE=promote-origin
 
-APPS=(indexer)
 GIT_REPO_URL=https://github.com/onlydustxyz/marketplace-indexer.git
 ENV_FILES="**/application.yaml Procfile*"
 
 check_args() {
-    if [[ -z $FROM_ENV || -z $TO_ENV ]]; then
+    if [[ -z $FROM_BRANCH || -z $TO_BRANCH ]]; then
       exit_error "Invalid arguments, you must specify at least --staging or --production flag"
     fi
 }
@@ -46,16 +41,6 @@ create_remote() {
     fi
 }
 
-check_commits() {
-    heroku_commit=$(heroku config:get HEROKU_SLUG_COMMIT --app "od-${APPS[0]}-$FROM_ENV")
-    git_commit=$(git rev-parse "$REMOTE/$FROM_BRANCH")
-    if [ "$heroku_commit" != "$git_commit" ]; then
-        echo -n "Heroku commit: " && git --no-pager log "$heroku_commit" --pretty=format:'%Cred%h%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --no-walk
-        echo -n "Git commit   : " && git --no-pager log "$git_commit" --pretty=format:'%Cred%h%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --no-walk
-        exit_error "Looks like what is currently deployed on Heroku does not match git branch"
-    fi
-}
-
 check_env_vars_diff() {
     log_info "Checking diff in environment variables"
     GIT_DIFF_CMD="git diff $to_commit..$from_commit -- $ENV_FILES"
@@ -74,14 +59,14 @@ git_push() {
     log_info "Pushing diff on git"
 
     if [ "$(git branch | grep -c $REMOTE)" -gt 0 ]; then
-      git branch -D $LOCAL_BRANCH
+      git branch -D "$LOCAL_BRANCH"
     fi
 
-    if ! git checkout -b $LOCAL_BRANCH "$REMOTE/$FROM_BRANCH"; then
+    if ! git checkout -b "$LOCAL_BRANCH" "$REMOTE/$FROM_BRANCH"; then
       exit_error "Unable to checkout $FROM_BRANCH to $LOCAL_BRANCH."
     fi
 
-    if ! git push $REMOTE "$LOCAL_BRANCH:$TO_BRANCH"; then
+    if ! git push "$GIT_FORCE" "$REMOTE" "$LOCAL_BRANCH:$TO_BRANCH"; then
       exit_error "Unable to push $FROM_BRANCH to $TO_BRANCH. Please rebase then try again."
     fi
 
@@ -89,36 +74,13 @@ git_push() {
     git branch -D $LOCAL_BRANCH
 }
 
-backup_database() {
-    log_info "Creating DB backup..."
-    execute heroku pg:backups:capture -a "od-${APPS[0]}-$TO_ENV"
-}
-
-promote_heroku() {
-    for app in "${APPS[@]}"
-    do
-        execute heroku pipelines:promote --app "od-$app-$FROM_ENV" --to "od-$app-$TO_ENV"
-    done
-
-    log_info "Waiting for dynos to be up..."
-    for url in "${HEALTH_CHECK_URLS[@]}"
-    do
-      while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' "$url")" != "200" ]]
-      do
-        echo "curl $url"
-        sleep 2
-      done
-    done
-}
-
 deploy() {
-    log_info "Retrieving apps infos..."
     from_commit=$REMOTE/$FROM_BRANCH
     to_commit=$REMOTE/$TO_BRANCH
 
     print "Checking diff from $to_commit to $from_commit"
 
-    log_info "Checking diff to be loaded in $TO_ENV"
+    log_info "Checking diff to be loaded in $TO_BRANCH"
     git log --color --graph --pretty=format:'%Cred%h%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' $to_commit..$from_commit | tee
 
     echo
@@ -130,48 +92,31 @@ deploy() {
 
     echo
     if ask "OK to continue"; then
-        if [ $SKIP_DB_DUMP -eq 0 ]
-        then
-            backup_database
-        fi
         git_push
-        promote_heroku
     fi
 }
 
 usage() {
-  echo "Usage: $0 [ --staging | --production ] [ --force ] [ --skip-db-dump ]"
+  echo "Usage: $0 [ --staging | --production ]"
   echo "  --staging         Promote to staging"
   echo "  --production      Promote to production"
-  echo "  --force           Force deploy even if commits are not the same"
-  echo "  --skip-db-dump    Skip database dump"
   echo ""
 }
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     --staging)
-      FROM_ENV=develop
       FROM_BRANCH=main
-      TO_ENV=staging
       TO_BRANCH=staging
-      HEALTH_CHECK_URLS=(https://staging-indexer.onlydust.com/actuator/health)
       shift
       ;;
     --production)
-      FROM_ENV=staging
       FROM_BRANCH=staging
-      TO_ENV=production
       TO_BRANCH=production
-      HEALTH_CHECK_URLS=(https://indexer.onlydust.com/actuator/health)
       shift
       ;;
     --force)
-      FORCE=1
-      shift
-      ;;
-    --skip-db-dump)
-      SKIP_DB_DUMP=1
+      GIT_FORCE=--force
       shift
       ;;
     --help | -h)
@@ -186,14 +131,9 @@ done
 
 check_args
 check_command git
-check_command heroku
+check_command aws
 check_cwd
 create_remote
-
-if [ $FORCE -eq 0 ]
-then
-    check_commits
-fi
 
 deploy
 
