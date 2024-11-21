cd8ec0b71424f44c3e0c5a4da04eac0c1300d4ac
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-11-21 12:05:16 +0100

    Add indexes suggested by Datadog

--- infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000065_add_missing_indexes.sql
+++ infrastructure/postgres-adapter/src/main/resources/db/changelog/changelogs/00000065_add_missing_indexes.sql
@@ -3,3 +3,10 @@ create unique index if not exists github_issues_labels_label_id_issue_id_uindex
 
 create index if not exists github_labels_name_index
     on indexer_exp.github_labels using gin (name public.gin_trgm_ops);
+
+-- Not related to the feature, but recommended by Datadog
+create unique index if not exists github_pull_requests_commits_pkey_inv
+    on indexer_exp.github_pull_requests_commits (pull_request_id, commit_sha);
+
+create index if not exists contributions_completed_at_main_file_extensions_status_type
+    on indexer_exp.contributions (completed_at, main_file_extensions, status, type);
