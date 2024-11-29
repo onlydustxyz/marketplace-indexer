a1c9c6e8dc137867fb8a0be8289bc971b7519e19
Anthony Buisset <abuisset@gmail.com>
2024-11-29 10:42:19 +0100

    increase thread pool size

--- infrastructure/aws-athena-adapter/src/main/java/com/onlydust/marketplace/indexer/infrastructure/aws_athena/AwsAthenaClient.java
+++ infrastructure/aws-athena-adapter/src/main/java/com/onlydust/marketplace/indexer/infrastructure/aws_athena/AwsAthenaClient.java
@@ -1,19 +1,28 @@
 package com.onlydust.marketplace.indexer.infrastructure.aws_athena;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.athena.AthenaClient;
-import software.amazon.awssdk.services.athena.model.*;
-import software.amazon.awssdk.services.athena.paginators.GetQueryResultsIterable;
+import static com.onlydust.marketplace.indexer.domain.exception.OnlyDustException.internalServerError;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static com.onlydust.marketplace.indexer.domain.exception.OnlyDustException.internalServerError;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.athena.AthenaClient;
+import software.amazon.awssdk.services.athena.model.AthenaException;
+import software.amazon.awssdk.services.athena.model.GetQueryExecutionRequest;
+import software.amazon.awssdk.services.athena.model.GetQueryResultsRequest;
+import software.amazon.awssdk.services.athena.model.QueryExecutionContext;
+import software.amazon.awssdk.services.athena.model.QueryExecutionStatus;
+import software.amazon.awssdk.services.athena.model.ResultConfiguration;
+import software.amazon.awssdk.services.athena.model.StartQueryExecutionRequest;
+import software.amazon.awssdk.services.athena.model.StartQueryExecutionResponse;
+import software.amazon.awssdk.services.athena.paginators.GetQueryResultsIterable;
 
+@Slf4j
 public class AwsAthenaClient {
     private final AthenaClient client;
     private final Properties properties;
@@ -52,20 +61,28 @@ public class AwsAthenaClient {
 
     private void pollForCompletion(final String queryExecutionId, CompletableFuture<GetQueryResultsIterable> results) {
         final var checkFuture = executor.scheduleAtFixedRate(() -> {
-            final var status = queryExecutionStatus(queryExecutionId);
-            switch (status.state()) {
-                case QUEUED:
-                case RUNNING:
-                    break;
-                case SUCCEEDED:
-                    results.complete(queryResults(queryExecutionId));
-                    break;
-                case FAILED:
-                    results.completeExceptionally(internalServerError("Query failed: " + status.athenaError().toString()));
-                    break;
-                case CANCELLED:
-                    results.completeExceptionally(new RuntimeException("Query cancelled"));
-                    break;
+            try {
+                final var status = queryExecutionStatus(queryExecutionId);
+                switch (status.state()) {
+                    case QUEUED:
+                    case RUNNING:
+                        break;
+                    case SUCCEEDED:
+                        results.complete(queryResults(queryExecutionId));
+                        break;
+                    case FAILED:
+                    case UNKNOWN_TO_SDK_VERSION:
+                        results.completeExceptionally(internalServerError("Query failed: " + status.athenaError().toString()));
+                        break;
+                    case CANCELLED:
+                        results.completeExceptionally(new RuntimeException("Query cancelled"));
+                        break;
+                }
+            } catch (AthenaException e) {
+                if (e.getMessage().contains("Rate exceeded"))
+                    LOGGER.warn("Rate exceeded, will retry in {} seconds: {}", properties.pollingInterval, e.getMessage());
+                else
+                    throw e;
             }
         }, 0, properties.pollingInterval, TimeUnit.SECONDS);
 
