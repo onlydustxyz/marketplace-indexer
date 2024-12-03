fce04e330c836d9cc515522dad7cae660bef0234
Anthony Buisset <abuisset@gmail.com>
2024-12-03 15:11:46 +0100

    properly exit application after batch ends

--- bootstrap/src/main/java/com/onlydust/marketplace/indexer/bootstrap/MarketplaceIndexerApplication.java
+++ bootstrap/src/main/java/com/onlydust/marketplace/indexer/bootstrap/MarketplaceIndexerApplication.java
@@ -5,7 +5,9 @@ import com.onlydust.marketplace.indexer.cron.JobScheduler;
 import com.onlydust.marketplace.indexer.github.GithubConfiguration;
 import com.onlydust.marketplace.indexer.postgres.PostgresConfiguration;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
@@ -17,10 +19,14 @@ import java.util.TimeZone;
 @EnableConfigurationProperties
 @EnableRetry
 @Import({PostgresConfiguration.class, GithubConfiguration.class, JobScheduler.class, BatchRunner.class})
+@Slf4j
 public class MarketplaceIndexerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MarketplaceIndexerApplication.class, args);
+        final var context = SpringApplication.run(MarketplaceIndexerApplication.class, args);
+
+        if (context.getEnvironment().getProperty("spring.main.web-application-type", WebApplicationType.class) == WebApplicationType.NONE)
+            System.exit(SpringApplication.exit(context));
     }
 
     @PostConstruct
