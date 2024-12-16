ff7ae0392ef8510f2439f1cfaad2ffcc45fe4646
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-12-16 13:47:28 +0100

    Use PostgresOutboxAdapter from kernel

--- infrastructure/postgres-adapter/src/main/java/com/onlydust/marketplace/indexer/postgres/repositories/ApiEventRepository.java
+++ infrastructure/postgres-adapter/src/main/java/com/onlydust/marketplace/indexer/postgres/repositories/ApiEventRepository.java
@@ -1,15 +1,17 @@
 package com.onlydust.marketplace.indexer.postgres.repositories;
 
-import com.onlydust.marketplace.indexer.postgres.OutboxRepository;
 import com.onlydust.marketplace.indexer.postgres.entities.ApiEventEntity;
+import io.hypersistence.utils.spring.repository.BaseJpaRepository;
+import lombok.NonNull;
+import onlydust.com.marketplace.kernel.infrastructure.postgres.OutboxRepository;
 import onlydust.com.marketplace.kernel.model.Event;
 
 import java.util.List;
 
-public interface ApiEventRepository extends OutboxRepository<ApiEventEntity> {
+public interface ApiEventRepository extends OutboxRepository<ApiEventEntity>, BaseJpaRepository<ApiEventEntity, Long> {
 
     @Override
-    default void saveEvent(Event event) {
+    default void saveEvent(@NonNull Event event) {
         persist(new ApiEventEntity(event));
     }
 
